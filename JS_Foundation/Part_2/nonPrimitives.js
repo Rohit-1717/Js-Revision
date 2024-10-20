aca9f73da4a8b90de0591484e3d6c706b56ed3a1
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2024-10-20 23:45:58 +0530

    Completed the concepts of Non - Primitive Data Types.

---
+++ JS_Foundation/Part_2/nonPrimitives.js
@@ -0,0 +1,44 @@
+//! Objects
+
+const username = {
+  " first Name": "Rohit",
+  isLoggedIn: true,
+};
+
+// const myFirstName = "Rohit";
+// myFirstName - "Mr. R";
+
+// console.log(username);
+// console.log(typeof username);
+
+// console.log(username.firstName);
+
+username.firstName = "Rohit Vishwakarma";
+
+// console.log(username);
+
+username.lastName = "Vishwakarma";
+
+// console.log(username);
+
+// console.log(username[" first Name"]);
+
+let today = new Date();
+
+// console.log(today.getDate());
+
+// console.log(today.getFullYear())
+
+//! Arrays
+
+let members = ["ROhit", true];
+
+// console.log(members[0]);
+// console.log(members[1]);
+
+let isValue= "234abc"; //true = 1 , false = 0
+// console.log(typeof  Number(isValue) );
+// console.log(typeof  Number(undefined) );
+
+console.log(typeof  Number(String) );
+
